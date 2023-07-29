bc6ce120b00f617a2088dd4188fb91f8c4acb066
root <eyobdagnahew23@gmail.com>
2023-07-29 00:27:37 -0400

    updated deploy script to support containerization update

--- deploy-site.sh
+++ deploy-site.sh
@@ -5,16 +5,7 @@ echo "$PWD"
 
 git fetch && git reset origin/main --hard
 
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+docker compose -f docker-compose.prod.yml down
 
 
-if [[ -n "$VIRTUAL_ENV" ]]; then
-    echo "Python virtual environment is active."
-else
-    echo "Python virtual environment is not active."
-fi
-
-systemctl restart myportfolio
-
-systemctl status myportfolio
+docker compose -f docker-compose.prod.yml up -d --buildS
